17881158b96973e28c30ea9c848d5a47dc9b3bb3
ROCIA FERNANDES <rocia.fernandes@gmail.com>
2017-07-01 14:24:52 +0530

    MV features explored

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,29 @@
 //Create the module 
 var myApp = angular.module("myModule", []);  
+
+//JSbasic3.html
 // Creating the controller and registering with the module all done in one line. 
 myApp.controller("myController", function ($scope) {     
 $scope.message = "AngularJS Tutorial"; 
 });  
+
+//JSbasic4.html
+myApp.controller("myCont", function ($scope) {
+  var employee = {         
+  firstName: 'Ben',         
+  lastName: 'Stokes',         
+  gender: 'Male' 
+  };  
+  $scope.employee = employee; 
+});  
+
+//JSbasic5.html
+myApp.controller("myCon", function ($scope) {
+  var country = {         
+  Country: 'France',         
+  Capital: 'Paris',         
+  Flag: 'France.png' 
+  };  
+  $scope.country = country; 
+});  
+
